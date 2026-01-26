61607b3b936f884d1823a6448d0499843c43a011
tiyo <eduardotiyo0@gmail.com>
2026-01-25 21:28:03 -0300

    feat: implement user update functionality with validation and error handling

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import type { Request } from 'express';
+import { UpdateUserDto } from './dto/update-user.dto';
+import type { AuthenticatedRequest } from 'src/auth/types/authenticated-request';
 
 @Controller('user')
 export class UserController {
@@ -16,9 +25,15 @@ export class UserController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  findAll(@Req() req: Request): Promise<User[]> {
+  findAll(@Req() req: AuthenticatedRequest): Promise<User[]> {
     console.log(req.user);
 
     return this.userService.findAll();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Patch('me')
+  update(@Body() dto: UpdateUserDto, @Req() req: AuthenticatedRequest) {
+    return this.userService.update(req.user.id, dto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,15 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { HashingService } from 'src/common/hashing/hashing.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -13,16 +19,26 @@ export class UserService {
     private readonly hashingService: HashingService,
   ) {}
 
-  async create(dto: CreateUserDto): Promise<User> {
-    const exists = await this.userRepository.exists({
-      where: {
-        email: dto.email,
-      },
-    });
+  async failIfEmailExists(email: string) {
+    const exists = await this.userRepository.existsBy({ email });
 
     if (exists) {
       throw new ConflictException('Email já existe.');
     }
+  }
+
+  async findOneByOrFail(userData: Partial<User>) {
+    const user = await this.userRepository.findOneBy(userData);
+
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado');
+    }
+
+    return user;
+  }
+
+  async create(dto: CreateUserDto): Promise<User> {
+    await this.failIfEmailExists(dto.email);
 
     const hashedPassword = await this.hashingService.hash(dto.password);
     const newUser: CreateUserDto = {
@@ -51,5 +67,23 @@ export class UserService {
 
   async findAll(): Promise<User[]> {
     return await this.userRepository.find();
+  }
+
+  async update(id: string, dto: UpdateUserDto) {
+    if (!dto.email && !dto.name) {
+      throw new BadRequestException('Dados não enviados');
+    }
+
+    const user = await this.findOneByOrFail({ id });
+
+    user.name = dto.name ?? user.name;
+
+    if (dto.email && dto.email !== user.email) {
+      await this.failIfEmailExists(dto.email);
+      user.email = dto.email;
+      user.forceLogout = true;
+    }
+
+    return this.save(user);
   }
 }
